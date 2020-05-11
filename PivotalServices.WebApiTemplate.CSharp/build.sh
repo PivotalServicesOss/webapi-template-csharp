875e982cda43ff56ee4f75eed917c63fcb70e2ef
David Wu <dawu@pivotal.io>
2020-05-11 12:08:40 -0500

    Fix the return of $psake.build_success variable
    Running in OSX causes $psake to be evaluated by Bash's environment variable, which ends up being blank.
    Two changes are proposed here:
    1) Use Powershell function to get the build_success value
    2) Move the app name as a variable within this file for ease of editing.

--- PivotalServices.WebApiTemplate.CSharp/build.sh
+++ PivotalServices.WebApiTemplate.CSharp/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+app_name="PivotalServices.WebApiTemplate.CSharp"
 echo Executing build with default.ps1 configuration
 pwsh -NoProfile -ExecutionPolicy bypass -Command "& {.\configure-build.ps1 }"
-pwsh -NoProfile -ExecutionPolicy bypass -Command "& {Invoke-psake .\default.ps1 $1 -parameters @{'project_name'='PivotalServices.WebApiTemplate.CSharp'}; exit !('$psake.build_success') }"
+pwsh -NoProfile -ExecutionPolicy bypass -Command "& {Invoke-psake .\default.ps1 $1 -parameters @{'project_name'='$app_name'}; exit !((GET-Variable psake -valueOnly).build_success) }"
