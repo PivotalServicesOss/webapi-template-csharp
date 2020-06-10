c849536ac899520ac43ef2c7f38e46f58f655ce1
Cedric Yao <cedric.yao@fiserv.net>
2020-06-10 09:08:26 -0600

    Fix issue where failing tasks return something other than success; Add assembly versioning as second parameter to build

--- PivotalServices.WebApiTemplate.CSharp/build.sh
+++ PivotalServices.WebApiTemplate.CSharp/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [$2 -eq ""] 
+then
+	assemblyVersion="1.0.0.0" 
+else 
+	assemblyVersion=$2
+fi
+
 echo Executing build with default.ps1 configuration
 pwsh -NoProfile -ExecutionPolicy bypass -Command "& {.\configure-build.ps1 }"
-pwsh -NoProfile -ExecutionPolicy bypass -Command "& {Invoke-psake .\default.ps1 $1 -parameters @{'project_name'='PivotalServices.WebApiTemplate.CSharp'}; exit !('$psake.build_success') }"
+pwsh -NoProfile -ExecutionPolicy bypass -Command "& {Invoke-psake .\default.ps1 $1 -parameters @{'project_name'='BillAccountValidation.WebApi'; \"assemblyVersion\"=\"$assemblyVersion\"}; exit !('$psake.build_success') }"
